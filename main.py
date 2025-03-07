f630fbb9d05bf18a6e44931ac8f42f086f5f377c
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2025-03-07 10:14:41 +0100

    remove unused files

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import argparse
 import threading
 import os
+from lib.visualize import plot_generations, init_pygame, draw_world, plot_biomass
+import pygame
 from lib.data_manager import data_loop, update_generations_data, process_data
 from lib.constants import override_from_file
-from lib.runners.single_agent_gym import SingleAgentGymRunner
-from lib.runners.petting_zoo_reinforcement import MultiAgentRunner
+# from lib.runners.single_agent_gym import SingleAgentGymRunner
+# from lib.runners.petting_zoo_reinforcement_old import RLRunner
+import lib.environments.gym
 from lib.runners.petting_zoo import PettingZooRunner
+from lib.runners.petting_zoo_single import PettingZooRunnerSingle
+from lib.runners.reinforcement import RLRunner
+from lib.runners.single_agent_gym import SingleAgentGymRunner
 import lib.constants as const
 from lib.model import Model
-import lib.environments.gym
 from lib.runner import Runner
-import torch
 import random
 import gymnasium as gym
 
@@ -34,43 +38,73 @@ if __name__ == "__main__":
     # Parse arguments
     args = parser.parse_args()
 
+    if args.agent_file:
+        print(f"Loading agent from file: {args.agent_file}")
+        runner = Runner()
+        screen = init_pygame()
+        def visualize(world, world_data, fitness):
+            agents_data = process_data({
+                'agent_index': 0,
+                'eval_index': 0,
+                'step': fitness,
+                'world': world
+            })
+            draw_world(screen, world, world_data)
+            plot_biomass(agents_data)
+            pygame.display.flip()
+            pygame.time.wait(1)
+        runner.simulate(agent_file=args.agent_file, visualize=visualize)
+
     # Load config files
     config_files = load_config_files(args.config_folder)
 
     print("Running simulation with the following config files:" + str(config_files))
 
-    with torch.no_grad():
-        for config_file in config_files:
-            override_from_file(config_file)
+    for config_file in config_files:
+        override_from_file(config_file)
+        print("start training")
+
+        if const.RUNNER == "rl_runner":
+            runner = RLRunner()
+            runner.train()
+        elif const.RUNNER == "petting_zoo":
             runner = PettingZooRunner()
             runner.train()
-            # simulation_thread = threading.Thread(target=runner.run)
-            # simulation_thread.start()
+        elif const.RUNNER == "petting_zoo_single":
+            runner = PettingZooRunnerSingle()
+            runner.train()
+        elif const.RUNNER == "gym":
+            runner = SingleAgentGymRunner()
+            runner.train()
+        elif const.RUNNER == "old":
+            runner = Runner()
+            simulation_thread = threading.Thread(target=runner.run)
+            simulation_thread.start()
             
-            # running = True
-            # while running and runner.current_generation < const.GENERATIONS_PER_RUN:
-            #     # Handle Pygame events
-            #     # for event in pygame.event.get():
-            #     #     if event.type == pygame.QUIT:
-            #     #         running = False
-            #     #         break
-            #     # Process visualization data
-            #     if hasattr(runner, 'data_queue'):
-            #         data_loop(runner.data_queue)
+            running = True
+            while running and runner.current_generation < const.GENERATIONS_PER_RUN:
+                # Handle Pygame events
+                # for event in pygame.event.get():
+                #     if event.type == pygame.QUIT:
+                #         running = False
+                #         break
+                # Process visualization data
+                if hasattr(runner, 'data_queue'):
+                    data_loop(runner.data_queue)
 
-            #     if runner.generation_finished.is_set():
-            #         runner.next_generation()
-            #         generations_data = update_generations_data(runner.current_generation)
-            #         # plot_generations(generations_data)
+                if runner.generation_finished.is_set():
+                    runner.next_generation()
+                    generations_data = update_generations_data(runner.current_generation)
+                    plot_generations(generations_data)
 
-            #         evaluation_thread = threading.Thread(target=runner.run)
-            #         evaluation_thread.start()
+                    evaluation_thread = threading.Thread(target=runner.run)
+                    evaluation_thread.start()
 
-            # runner = SingleAgentGymRunner()
+            simulation_thread.join()
 
-            # while runner.current_generation < const.GENERATIONS_PER_RUN:
-            #     runner.run_generation()
-            #     print(f"Generation {runner.current_generation} finished")
+
+
+        
 
 
     
